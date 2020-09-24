ab062846e590e2cc2a84820294c76fcb56e606f8
Rajneesh Yadav <yadavrajneesh999@gmail.com>
2020-09-24 14:48:24 +0530

    Fix Bug, Add Intent and Add Stories

--- README.md
+++ README.md
@@ -14,15 +14,4 @@ WTAPI is Flask based Api.
 
 1. After successfully run the code you have to open the **Postman**, you can download postman from here [Download Postman](https://www.postman.com/downloads/ "Download Postman").
 
-1. Then enter these command in url bar:-
-	- For create data in Database[PUT]:-
-	
-    		http://127.0.0.1:5000/createintent/YourID?name=Your&position=PositionName&experience=ExperienceInYear
-
-	- For List data from Database[GET]:-
-	
-			http://127.0.0.1:5000/listintent/YourId
-
-	- For Update data in Database[PATCH]:-
-	
-			http://127.0.0.1:5000/updateintent/YourID?FieldName=Value
+1. We'll also share with you postman json api open it in postman collections.
--- app.py
+++ app.py
@@ -18,6 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
 db = SQLAlchemy(app)
 
+
 #############################
 ## intents argument handle ##
 #############################
@@ -26,7 +27,7 @@ class IntentModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     intent_name = db.Column(db.String(50), nullable=False)
     intent_description = db.Column(db.String(100), nullable=False)
-    bot_id = db.Column(db.Integer, db.ForeignKey("create_bot_model.id"), nullable=False)
+    bot_id = db.Column(db.Integer, db.ForeignKey("bot_model.id"), nullable=False)
 
     def __repr__(self):
         # return '<User %r>' % self.name
@@ -58,36 +59,37 @@ resources_fields_intent = {
 class StoryModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     story_name = db.Column(db.String(50), nullable=False)
-    intent_story = db.Column(db.String(100), nullable=False)
+    intent_id = db.Column(db.Integer, db.ForeignKey("intent_model.id"), nullable=False)
     action_name = db.Column(db.String(100), nullable=False)
-    bot_id = db.Column(db.Integer, nullable=False)
+    bot_id = db.Column(db.Integer, db.ForeignKey("bot_model.id"), nullable=False)
 
     def __repr__(self):
         # return '<User %r>' % self.name
-        return f'CreateStories(story_name={story_name}, intent_story={intent_story}, action_name={action_name}, bot_id={bot_id} )'
+        return f'CreateStories(story_name={story_name}, intent_id={intent_id}, action_name={action_name}, bot_id={bot_id} )'
 
 
 stories_put_args = reqparse.RequestParser()
 stories_put_args.add_argument("story_name", type=str, help="Name of Stories is Required", required=True)
-stories_put_args.add_argument("intent_story", type=str, help="Intent Should be Mapped to Stories", required=True)
+stories_put_args.add_argument("intent_id", type=int, help="Intent Should be Mapped to Stories", required=True)
 stories_put_args.add_argument("action_name", type=str, help="Action Should be Mapped to Intents for a Story",
                               required=True)
 stories_put_args.add_argument("bot_id", type=int, help="Action Should be Mapped to Intents for a Story", required=True)
 
 stories_update_args = reqparse.RequestParser()
 stories_update_args.add_argument("story_name", type=str, help="Name of Stories is Required")
-stories_update_args.add_argument("intent_story", type=str, help="Intent Should be Mapped to Stories")
+stories_update_args.add_argument("intent_id", type=int, help="Intent Should be Mapped to Stories")
 stories_update_args.add_argument("action_name", type=str, help="Action Should be Mapped to Intents for a Story")
 stories_update_args.add_argument("bot_id", type=int, help="Action Should be Mapped to Intents for a Story")
 
 resources_fields_story = {
     'id': fields.Integer,
     'story_name': fields.String,
-    'intent_story': fields.String,
+    'intent_id': fields.Integer,
     'action_name': fields.String,
     'bot_id': fields.Integer,
 
 }
+
 
 ##########################
 ## bot arguments handle ##
@@ -102,7 +104,7 @@ resources_fields_story = {
 #         return f'TrainBot(bot_id={bot_id})'
 
 
-class CreateBotModel(db.Model):
+class BotModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     bot_name = db.Column(db.String(50), nullable=False)
 
@@ -123,7 +125,6 @@ resources_fields_bot = {
 
 # db.create_all()
 
-
 #########
 # Views #
 #########
@@ -133,28 +134,36 @@ class CreateIntents(Resource):
     def put(self, intent_id):
         args = intent_put_args.parse_args()
         result = IntentModel.query.filter_by(id=intent_id).first()
+
         if result:
             abort(409, message="Intent is Taken")
+
         intent = IntentModel(
             id=intent_id,
             intent_name=args['intent_name'],
             intent_description=args['intent_description'],
             bot_id=args['bot_id']
         )
+        bot_i = BotModel.query.filter_by(id=intent.bot_id).first()
+        bot_name = bot_i.bot_name
+        try:
+            pathnlu = bot_name + "/data/nlu.md"
+            print("Bot id is", args['bot_id'])
+            if str(os.path.exists(pathnlu)):
+                f = open(pathnlu, "a")
+                f.write("\n")
+                f.write("## intent:")
+                f.write(intent.intent_name)
+                f.write("\n")
+                f.write("- ")
+                f.write(intent.intent_description)
+                f.close()
+                print("Intent ", intent.intent_name, " Created ")
+            else:
+                print("Unable to Create Intent")
 
-        # pathnlu = str(args['bot_id']) + "/nlu.md"  # 999/nlu.md
-        #
-        # print("Bot id is", args['bot_id'])
-        # if str(path.exists(pathnlu)):
-        #     f = open(pathnlu, "a")
-        #     f.write("* ")
-        #     f.write(args['intent_name'])
-        #     f.write("\n")
-        #     f.write("-  ")
-        #     f.write(args['intent_description'])
-        #     f.close()
-        # else:
-        #     abort(409, message="Bot Doesn't Exist")
+        except FileExistsError:
+            print("Bot ", intent.bot_id, " Doesn't Exist")
 
         db.session.add(intent)
         db.session.commit()
@@ -180,12 +189,13 @@ class UpdateIntent(Resource):
             abort(404, message="Intent was not found, can not update")
 
         if args['intent_name']:
-            result.name = args['intent_name']
+            result.intent_name = args['intent_name']
         if args['intent_description']:
-            result.position = args['intent_description']
+            result.intent_description = args['intent_description']
 
         db.session.commit()
         return result
+
 
 class CreateStories(Resource):
     @marshal_with(resources_fields_story)
@@ -197,20 +207,36 @@ class CreateStories(Resource):
         story = StoryModel(
             id=story_id,
             story_name=args['story_name'],
-            intent_story=args['intent_story'],
+            intent_id=args['intent_id'],
             action_name=args['action_name'],
             bot_id=args['bot_id']
         )
+        bot_i = BotModel.query.filter_by(id=story.bot_id).first()
+        bot_name = bot_i.bot_name
+        print("Bot id is", args['bot_id'])
+        pathstories = bot_name + "/data/stories.md"
+        intent_i = IntentModel.query.filter_by(id=story.intent_id).first()
+        intent_name = intent_i.intent_name
+        try:
+            if str(os.path.exists(pathstories)):
+                f = open(pathstories, "a")
+                f.write("\n")
+                f.write("## ")
+                f.write(story.story_name)
+                f.write("\n")
+                f.write("* ")
+                f.write(intent_name)
+                f.write("\n")
+                f.write("  - ")
+                f.write(story.action_name)
+                f.close()
+                print("Story ", story.story_name, " Created ")
+            else:
+                print("Unable to Create Story")
 
-        pathstories = str(args['bot_id']) + "/stories.md"  # 999/stories.md
+        except FileExistsError:
+            print("Bot ", story.bot_id, " Doesn't Exist")
 
-        f = open(pathstories, "a")
-        f.write("## ")
-        f.write(args['story_name'])
-        f.write("\n")
-        f.write("-  ")
-        f.write(args['action_name'])
-        f.close()
 
         db.session.add(story)
         db.session.commit()
@@ -222,12 +248,12 @@ class CreateBot(Resource):
     def put(self, bot_id):
         args = bot_put_args_bot.parse_args()
 
-        result = CreateBotModel.query.filter_by(id=bot_id).first()
+        result = BotModel.query.filter_by(id=bot_id).first()
 
         if result:
             abort(409, message="Bot is Taken")
 
-        bot = CreateBotModel(id=bot_id, bot_name=args['bot_name'])
+        bot = BotModel(id=bot_id, bot_name=args['bot_name'])
 
         db.session.add(bot)
         db.session.commit()
@@ -235,15 +261,11 @@ class CreateBot(Resource):
         dirName = str(bot.bot_name)
 
         try:
-            # Create target Directory
             os.mkdir(dirName)
             print("Directory ", dirName, " Created ")
 
-            # sample_bot = os.path.dirname('./sample_bot')
-
             for root, dirs, files in os.walk('sample_bot'):
                 for file in files:
-
                     path_file = os.path.join(root, file)
                     shutil.copy2(path_file, dirName)
 
@@ -251,7 +273,7 @@ class CreateBot(Resource):
             path = os.path.join(dirName, directory)
             os.mkdir(path)
 
-            for filename in glob.glob(os.path.join(dirName,'*.md')):
+            for filename in glob.glob(os.path.join(dirName, '*.md')):
                 shutil.move(filename, path)
 
         except FileExistsError:

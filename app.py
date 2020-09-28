8763d40a81002430e48f97bf21ff7298228f25c5
Rajneesh Yadav <yadavrajneesh999@gmail.com>
2020-09-28 17:53:14 +0530

    Add Register intent and story feature and also train bot

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import subprocess
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
@@ -61,6 +62,7 @@ class StoryModel(db.Model):
     story_name = db.Column(db.String(50), nullable=False)
     intent_id = db.Column(db.Integer, db.ForeignKey("intent_model.id"), nullable=False)
     action_name = db.Column(db.String(100), nullable=False)
+    action_reply = db.Column(db.String(500), nullable=False)
     bot_id = db.Column(db.Integer, db.ForeignKey("bot_model.id"), nullable=False)
 
     def __repr__(self):
@@ -73,12 +75,15 @@ stories_put_args.add_argument("story_name", type=str, help="Name of Stories is R
 stories_put_args.add_argument("intent_id", type=int, help="Intent Should be Mapped to Stories", required=True)
 stories_put_args.add_argument("action_name", type=str, help="Action Should be Mapped to Intents for a Story",
                               required=True)
+stories_put_args.add_argument("action_reply", type=str, help="Action Should be Mapped to Intents for a Story",
+                              required=True)
 stories_put_args.add_argument("bot_id", type=int, help="Action Should be Mapped to Intents for a Story", required=True)
 
 stories_update_args = reqparse.RequestParser()
 stories_update_args.add_argument("story_name", type=str, help="Name of Stories is Required")
 stories_update_args.add_argument("intent_id", type=int, help="Intent Should be Mapped to Stories")
 stories_update_args.add_argument("action_name", type=str, help="Action Should be Mapped to Intents for a Story")
+stories_update_args.add_argument("action_reply", type=str, help="Action Should be Mapped to Intents for a Story")
 stories_update_args.add_argument("bot_id", type=int, help="Action Should be Mapped to Intents for a Story")
 
 resources_fields_story = {
@@ -116,6 +121,9 @@ class BotModel(db.Model):
 bot_put_args_bot = reqparse.RequestParser()
 bot_put_args_bot.add_argument("bot_name", type=str, help="Bot Name Required", required=True)
 
+bot_post_args_bot = reqparse.RequestParser()
+bot_post_args_bot.add_argument("bot_name", type=str, help="Bot Name Required", required=True)
+
 resources_fields_bot = {
     'bot_name': fields.String,
 }
@@ -144,8 +152,10 @@ class CreateIntents(Resource):
             intent_description=args['intent_description'],
             bot_id=args['bot_id']
         )
+
         bot_i = BotModel.query.filter_by(id=intent.bot_id).first()
         bot_name = bot_i.bot_name
+
         try:
             pathnlu = bot_name + "/data/nlu.md"
             print("Bot id is", args['bot_id'])
@@ -164,6 +174,27 @@ class CreateIntents(Resource):
 
         except FileExistsError:
             print("Bot ", intent.bot_id, " Doesn't Exist")
+
+        dpathstories = bot_name + "/domain.yml"
+
+        try:
+            if str(os.path.exists(dpathstories)):
+                f = open(dpathstories, "a")
+
+                f.write("\n")
+                f.write("intent:")
+                f.write("\n")
+                f.write("  - ")
+                f.write(intent.intent_name)
+                f.write("\n")
+                f.close()
+
+                print("Intent ", intent.intent_name, " is Registered")
+            else:
+                print("Unable to Register Intent")
+
+        except FileNotFoundError:
+            print("File Not Found")
 
         db.session.add(intent)
         db.session.commit()
@@ -209,6 +240,7 @@ class CreateStories(Resource):
             story_name=args['story_name'],
             intent_id=args['intent_id'],
             action_name=args['action_name'],
+            action_reply=args['action_reply'],
             bot_id=args['bot_id']
         )
         bot_i = BotModel.query.filter_by(id=story.bot_id).first()
@@ -237,6 +269,27 @@ class CreateStories(Resource):
         except FileExistsError:
             print("Bot ", story.bot_id, " Doesn't Exist")
 
+        dpathstories = bot_name + "/domain.yml"
+
+        try:
+            if str(os.path.exists(dpathstories)):
+                f = open(dpathstories, "a")
+                f.write("\n")
+                f.write("responses:")
+                f.write("\n")
+                f.write("  ")
+                f.write(story.action_name)
+                f.write(":")
+                f.write("\n")
+                f.write("  - text: ")
+                f.write(story.action_reply)
+                f.close()
+                print("Story ", story.story_name, " is Register")
+            else:
+                print("Unable to Register Story")
+
+        except FileNotFoundError:
+            print("File not Found")
 
         db.session.add(story)
         db.session.commit()
@@ -282,17 +335,25 @@ class CreateBot(Resource):
         return bot, 201
 
 
-# class TrainBot(Resource):
-#     @marshal_with(resources_fields_bot)
-#     def post(self, bot_id):
-#         result = BotModel.query.filter_by(bot_id=bot_id).first()
-#         if not result:
-#             abort(404, message="Bot Not Found")
-#
-#         os.system('cmd /c "rasa train"')
-#
-#         # train for particular bot
-#         return result
+class TrainBot(Resource):
+    @marshal_with(resources_fields_bot)
+    def post(self, bot_id):
+        args = bot_post_args_bot.parse_args()
+        result = BotModel.query.filter_by(id=bot_id).first()
+        if not result:
+            abort(404, message="Bot Not Found")
+
+        bot = BotModel(id=bot_id, bot_name=args['bot_name'])
+
+        dirName = str(bot.bot_name)
+
+        try:
+            subprocess.call('rasa train', shell=True, cwd=dirName)
+
+        except FileNotFoundError:
+            print("Directory ", dirName, " doesn't found")
+
+        return result
 
 
 # Add logic to deploy bot , execute rasa shell or rasa --endpoint
@@ -301,9 +362,9 @@ api.add_resource(CreateBot, "/createbot/<int:bot_id>")
 api.add_resource(CreateIntents, "/createintents/<int:intent_id>")
 api.add_resource(CreateStories, "/createstories/<int:story_id>")
 
-# api.add_resource(TrainBot, "/trainbot/<int:bot_id>")
+api.add_resource(TrainBot, "/trainbot/<int:bot_id>")
 
-api.add_resource(ListIntent, "/listintents/<int:intent_id>")
+api.add_resource(ListIntent, "/listintent/<int:intent_id>")
 api.add_resource(UpdateIntent, "/updateintent/<int:intent_id>")
 
 # Add logic to deploy bot action, rasa --endpoint for the new bot or specific id
